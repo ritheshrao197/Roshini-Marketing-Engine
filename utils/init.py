258fc0ca0943e49e97f118b778444aadfde0d0ed
Rithesh U <rithesh197rao@email.com>
2026-07-12 20:53:08 +0530

    fix: update import statements to use relative paths in init.py

--- utils/init.py
+++ utils/init.py
@@ -2,12 +2,12 @@
 Utility modules for the marketing pipeline.
 """
 
-from utils.logger import get_logger
-from utils.files import ensure_directory, ensure_file
-from utils.api import safe_request
-from utils.markdown import parse_markdown
-from utils.slug import generate_slug
-from utils.seo import optimize_seo
+from .logger import get_logger
+from .files import ensure_directory, ensure_file
+from .api import safe_request
+from .markdown import parse_markdown
+from .slug import generate_slug
+from .seo import optimize_seo
 
 __all__ = [
     'get_logger',
